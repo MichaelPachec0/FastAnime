878b886b5be7a485e96daa4764ae93a5deecf722
Michael Pacheco <git@michaelpacheco.org>
2025-07-25 19:58:21 -0700

    fix: add missing deps.
    * need to pull a official libtorrent whl so that it compiles.
    * also changes some of dep versions so that they match nixpkgs.
    
    TODO: still need to find the hashes for arm64 linux, darwin and x64
    darwin.
    WARN: darwin doesnt build since plyer is broken in nixpkgs.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- flake.nix
+++ flake.nix
@@ -11,6 +11,43 @@
       pkgs = import nixpkgs { inherit system; };
 
       python = pkgs.python312;
+      pyShortVersion = "cp${builtins.replaceStrings [ "." ] [ "" ] python.pythonVersion}";
+      platforms = {
+        # TODO: TRY AND FIX THIS?
+
+        # aarch64-darwin = {
+        #   platform = "macosx_15_0_arm64" ;
+        #   hash = pkgs.lib.fakeHash;
+        # };
+        #
+        # aarch64-linux =  { 
+        #   platform = "manylinux_2_17_aarch64.manylinux2014_aarch64";
+        #   hash = pkgs.lib.fakeHash;
+        # };
+        # x86_64-darwin = { 
+        #   platform = "macosx_15_0_x86_64";
+        #   hash = pkgs.lib.fakeHash;
+        # };
+        x86_64-linux = {
+          platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+          hash = "sha256-+/RBhHGpYVjZlcRXNpfOLc8nykLQsDw7+XsBZQFb9kE=";
+        };
+      };
+      platform = platforms.${pkgs.system}.platform or (throw "Unsupported system: ${pkgs.system}");
+      hash = platforms.${pkgs.system}.hash;
+      
+      libtorrent = python.pkgs.buildPythonPackage rec {
+        pname = "libtorrent";
+        version = "2.0.11";
+        format = "wheel";
+
+        src = pkgs.fetchPypi {
+          inherit pname version format platform hash;
+          dist = pyShortVersion;
+          python = pyShortVersion;
+          abi = pyShortVersion;
+        };
+      };
       pythonPackages = python.pkgs;
       fastanimeEnv = pythonPackages.buildPythonApplication {
         pname = "fastanime";
@@ -21,6 +58,8 @@
         preBuild = ''
           sed -i 's/rich>=13.9.2/rich>=13.8.1/' pyproject.toml
           sed -i 's/pycryptodome>=3.21.0/pycryptodome>=3.20.0/' pyproject.toml
+          sed -i 's/pydantic>=2.11.7/pydantic>=2.11.4/' pyproject.toml
+          sed -i 's/lxml>=6.0.0/lxml>=5.4.0/' pyproject.toml
         '';
 
         # Add runtime dependencies
@@ -38,7 +77,13 @@
           fastapi
           pycryptodome
           pypresence
-        ];
+          beautifulsoup4
+          httpx
+          libtorrent
+          lxml
+        ] ++ (with pkgs; [
+            fzf
+          ]);
 
         # Ensure compatibility with the pyproject.toml
         format = "pyproject";
